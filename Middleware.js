5a6d00fa866269bf9096b2a20cb0c551611e66da
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-10-06 13:06:14 +0200

    Add files via upload

---
+++ Middleware.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+function jsonReader(filePath, cb){
+    fs.readFile(filePath ,'utf-8',(err, fileData) => {
+        if(err){
+            return cb && cb(err);
+        }
+        try{
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        }catch(err){
+            return cb && cb(err);
+        }
+    });
+}
+
+jsonReader('',(err,data) => {
+    if (err){
+        console.log(err)
+    }else{
+        console.log(data.adress);
+    }
+});
---
+++ Singleton.js
@@ -0,0 +1,26 @@
+/*
+Construeixi una aplicació que creï diversos jugadors.
+
+Els jugadors podran ser afegits a un Joc, que mostrarà un marcador amb les puntuacions i el guanyador. 
+
+Podrà fixar en cada jugador punts guanyats o perduts perquè el marcador canviï. 
+
+La classe Marcador deurà, com a requisit indispensable, implementar un patró Singleton.
+*/
+
+class Joc{
+
+}
+
+class Jugador{
+    constructor(nom, punts){
+        this.nom=nom;
+        this.punts=punts;
+    }
+
+}
+
+class Marcador{
+    constructor()
+    
+}
