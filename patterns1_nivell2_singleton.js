cff905baa33122176913ac355ebdd3524f5d4bf5
Daniel Acevedo <dlacevedo@protonmail.com>
2021-10-08 19:40:04 +0200

    Update patterns1_nivell2_singleton.js

--- patterns1_nivell2_singleton.js
+++ patterns1_nivell2_singleton.js
@@ -6,21 +6,13 @@ class Marcador{
             return console.log(`El joc ${_joc.nom} no existeix.`);
         }
 
-        let maxim;
-        for(let i=0; i<_joc.punts.length; i++){
-
-            if(_joc.punts[i] || _joc.punts[i]==0){
-                if(!maxim && maxim != 0){
-                    maxim = _joc.punts[i];
-                }else{
-                    maxim = Math.max(maxim, _joc.punts[i]);
-                }   
-            }
-
+        if(_joc.punts.length == 0){
+            return console.log(`El joc ${_joc.nom} no té guanyador.`)
         }
 
-        if(!maxim && maxim!=0){
-            return console.log(`El joc ${_joc.nom} no té cap guanyador.`)
+        let maxim = _joc.punts[0];
+        for(let i=1; i<_joc.punts.length; i++){
+            maxim = Math.max(maxim, _joc.punts[i]);
         }
 
         let guanyadors = [];
@@ -34,7 +26,31 @@ class Marcador{
     }
 
     mostrarPuntuacions(_joc){
-        //Afegir Codi per crear parells nomjugador:puntuació i ordenar-los de major a menor.
+
+        if(!(_joc instanceof Joc)){
+            return console.log(`El joc ${_joc.nom} no existeix.`);
+        }
+
+        if(_joc.jugadors.length == 0){
+            return console.log(`El joc ${_joc.nom} no té jugadors.`)
+        }
+        
+        let parells = {};
+        _joc.jugadors.forEach((_jugador, i) => parells[_jugador.nom] = _joc.punts[i]);
+        
+        let ranking = [];
+        for (let key in parells) {
+            ranking.push([key, parells[key]]);
+        }
+
+        ranking.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        console.log(`Classificació del joc ${_joc.nom}:`);
+        for(let i=0; i<ranking.length; i++){
+            console.log(`${ranking[i][0]} amb ${ranking[i][1]} punts.`);
+        }
     }
 }
 
@@ -47,6 +63,9 @@ class Jugador{
 let jugador1 = new Jugador("Manu");
 let jugador2 = new Jugador("Dani");
 let jugador3 = new Jugador("Sergi");
+let jugador4 = new Jugador("Javi");
+let jugador5 = new Jugador("Roger");
+let jugador6 = new Jugador("Joan");
 
 class Joc{
     constructor(_nom){
@@ -66,7 +85,10 @@ class Joc{
                 return console.log(`${_noujugador.nom} ja és a ${this.nom}`)
             }
         }
-        return this.jugadors.push(_noujugador);
+
+        this.jugadors.push(_noujugador);
+        this.punts.push(0);
+        console.log(`S'ha afegit a ${_noujugador.nom} al joc ${this.nom} amb una puntuació inicial de zero.`);
     }
 
     afegirPunts(_jugador, _punts){
@@ -76,54 +98,55 @@ class Joc{
         }
 
         for(let i=0; i<this.jugadors.length; i++){
-            
             if (_jugador == this.jugadors[i]){
-                if (!this.punts[i]){
-                    return this.punts[i] = _punts;
-                }
-                return this.punts[i] = this.punts[i] + _punts;
+                this.punts[i] = this.punts[i] + _punts;
+                return console.log(`S'ha afegit ${_punts} punt(s) a ${_jugador.nom}. La seva puntuació ara és de ${this.punts[i]} punt(s).`);
             }
-
         }
-        return console.log(`${_jugador.nom} no és al joc ${this.nom} `);
+
+        return console.log(`${_jugador.nom} no és al joc ${this.nom}. `);
     }
     
     mostrarMarcador(){
         var marcador = new Marcador;
-        //marcador.mostrarPuntuacions(this);
+        marcador.mostrarPuntuacions(this);
         marcador.mostrarGuanyador(this);
     }
 }
 
 let joc1 = new Joc("Monster Hunter");
 let joc2 = new Joc("Magic The Gathering");
-let joc3 = new Joc("parxis");
+let joc3 = new Joc("Fortnite");
 
 joc1.afegirJugador(jugador1);
 joc1.afegirJugador(jugador2);
+joc1.afegirJugador(jugador4);
+joc1.afegirJugador(jugador5);
+
 joc2.afegirJugador(jugador2);
 joc2.afegirJugador(jugador3);
+joc2.afegirJugador(jugador6);
 
-joc1.afegirPunts(jugador1, 11);
-joc1.afegirPunts(jugador2, 12);
-joc1.afegirPunts(jugador3, 13);
-joc2.afegirPunts(jugador1, 21);
-joc2.afegirPunts(jugador2, 22);
-joc2.afegirPunts(jugador3, 23);
+joc1.afegirPunts(jugador1, 5);
+joc1.afegirPunts(jugador2, 13);
+joc1.afegirPunts(jugador3, 3);
+joc1.afegirPunts(jugador4, 13);
+joc1.afegirPunts(jugador5, 9);
 
-console.log(jugador1);
-console.log(jugador2);
-console.log(jugador3);
+joc2.afegirPunts(jugador2, 5);
+joc2.afegirPunts(jugador3, 4);
+joc2.afegirPunts(jugador6, 20);
+
 console.log(joc1);
 console.log(joc2);
 
 
 joc1.mostrarMarcador();
+joc2.mostrarMarcador();
+joc3.mostrarMarcador();
 
 
-//Ara mateix: quan s'assigna un jugador a un joc, aquest no té cap valor de "punts" fins que no se li n'afegeixen amb afegirPunts, si és que es fa.
-//possibilitat: assignar d'entrada zero punts als jugadors amb el method afegirJugadors. té sentit que un jugador pertanyi a un joc i tingui el camp de punts buit?
-//Conseqüència: simplificar funcions que manipulen l'array de puntuacions, perquè ja no han de tenir en compte els valors no definits.
+//simplificar mostrarGuanyador, perquè ja no ha de tenir en compte els valors no definits.(son zero)
 
 //Implementar singleton
 
