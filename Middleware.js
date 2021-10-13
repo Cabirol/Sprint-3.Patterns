e72b43eff5e7dff22707110f07636e9cfe6e3c69
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-10-13 12:59:08 +0200

    Update Middleware.js

--- Middleware.js
+++ Middleware.js
@@ -21,3 +21,26 @@ jsonReader('',(err,data) => {
         console.log(data.adress);
     }
 });
+
+function sumar(){
+    let suma = 0;
+    for (let i=0; i<arguments.length; i++){
+        suma += arguments[i];
+    }
+    return suma;
+}
+
+function restar(minuend, substraend){
+    return minuend - substraend;
+}
+
+
+function multiplicar(){
+    let mult = 1;
+    for (let i=0; i<arguments.length; i++){
+        mult *= arguments[i];
+    }
+    return mult;
+}
+
+module.exports = {sumar, restar, multiplicar};
